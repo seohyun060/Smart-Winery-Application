e0a2bde43154e8a929a019a8d235e1b951980c7d
Noah <seohyun060@naver.com>
2023-07-11 15:09:00 +0530

    init

---
+++ app/src/main/java/com/example/smart_winery/LoginPage.kt
@@ -0,0 +1,15 @@
+package com.example.smart_winery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class LoginPage : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+    }
+}
