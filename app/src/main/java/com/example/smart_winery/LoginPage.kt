b7aaac178232e0c2bba4a2a005b67bebed188597
Noah <seohyun060@naver.com>
2023-07-12 15:06:18 +0530

    login_page kt publishing

--- app/src/main/java/com/example/smart_winery/LoginPage.kt
+++ app/src/main/java/com/example/smart_winery/LoginPage.kt
@@ -1,15 +1,20 @@
 package com.example.smart_winery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smart_winery.databinding.LoginPageBinding
 
 
 class LoginPage : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        val loginPageBinding = LoginPageBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_page)
+        setContentView(loginPageBinding.root)
+       loginPageBinding.loginNaver.setOnClickListener {
+           
+       }
     }
 }
