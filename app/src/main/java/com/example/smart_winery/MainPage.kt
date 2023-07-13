cd682115fef5ef3e60ab574fa1466a84b9e2ce05
Wonski <ricky2101@naver.com>
2023-07-13 15:04:21 +0530

    build

--- app/src/main/java/com/example/smart_winery/MainPage.kt
+++ app/src/main/java/com/example/smart_winery/MainPage.kt
@@ -16,6 +16,29 @@ class MainPage : AppCompatActivity() {
         val binding = MainPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val firstfloor = arrayListOf<ImageButton>(
+            findViewById(R.id.btn11),
+            findViewById(R.id.btn12),
+            findViewById(R.id.btn13),
+            findViewById(R.id.btn14),
+            findViewById(R.id.btn15)
+        )
+        val secondfloor = arrayListOf<ImageButton>(
+            findViewById(R.id.btn21),
+            findViewById(R.id.btn22),
+            findViewById(R.id.btn23),
+            findViewById(R.id.btn24),
+            findViewById(R.id.btn25)
+        )
+        val thirdfloor = arrayListOf<ImageButton>(
+            findViewById(R.id.btn31),
+            findViewById(R.id.btn32),
+            findViewById(R.id.btn33),
+            findViewById(R.id.btn34),
+            findViewById(R.id.btn35)
+        )
+
+
 
     }
 }
